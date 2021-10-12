e5c52d3a1a33879ee231101883d11149da366c28
Lucas Batista <batista.lucas3098@gmail.com>
2021-10-12 10:33:10 -0300

    map construction

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ const map = [
     "W       W       W   W",
     "WWWWWWWWWWWWWWWWWWWWW",
 ];
+
+const mapSection = document.getElementById('map')
+
+for (let i = 0; i < map.length; i++) {
+    let linha = document.createElement('div')
+    linha.id = `linha${i}`
+    linha.classList.add('linha')
+    console.log(linha)
+    for (let j = 0; j < map[i].length; j++) {
+        let celula = document.createElement('div');
+        celula.id = `celula${j}`
+        celula.classList.add('celula')
+        linha.appendChild(celula)
+        if (map[i][j] === 'W') {
+            celula.style.backgroundColor = "#0000FF"
+        }
+    }
+    mapSection.appendChild(linha)
+
+}
